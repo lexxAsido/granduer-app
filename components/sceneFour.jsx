bd6f3bd1756ab5b8af67f85f61b7a9f0f873a0e5
lexxAsido <alexandaras2015@gmail.com>
2025-01-02 12:15:28 +0100

    latest commit

--- components/sceneFour.jsx
+++ components/sceneFour.jsx
@@ -42,14 +42,13 @@ const SceneFour = () => {
       <div className="relative flex justify-center items-center bg-gray-100 py-5 shadow-lg shadow-black p-2 border 
         w-[80vw] h-[80vw] md:w-[60vw] md:h-[60vw] lg:w-[50rem] lg:h-[50rem] max-md:mb-5">
 
-        {/* Central App */}
+       
         <div className="central-app flex flex-col items-center gap-3 justify-center">
           <RiSignalTowerFill className="text-3xl animate-ping" />
-          <FaMobileAlt size={100} className="text-cyan-500" />
+          <FaMobileAlt  className="text-cyan-500 text-9xl max-lg:text-6xl" />
           <h3 className="font-bold text-xl text-center max-lg:text-sm">GrandeurSmart App</h3>
         </div>
 
-        {/* SVG for arrows */}
         <svg className="absolute w-full h-full" xmlns="http://www.w3.org/2000/svg">
           <line
             id="arrow-light"
@@ -133,30 +132,30 @@ const SceneFour = () => {
           />
         </svg>
 
-        {/* Devices */}
+        
         <div className="absolute top-5 left-5">
-          <FaLightbulb size={50} className="text-yellow-500 animate-pulse" />
+          <FaLightbulb  className="text-yellow-500 animate-pulse text-7xl max-lg:text-4xl" />
         </div>
         <div className="absolute top-5 right-50">
-          <FaCamera size={50} className="text-black animate-pulse" />
+          <FaCamera className="text-black animate-pulse text-7xl max-lg:text-4xl" />
         </div>
         <div className="absolute bottom-5 left-50">
-          <GiTheaterCurtains size={50} className="text-blue-700 animate-pulse" />
+          <GiTheaterCurtains  className="text-blue-700 animate-pulse text-7xl max-lg:text-4xl " />
         </div>
         <div className="absolute top-5 right-5">
-          <FaThermometerHalf size={50} className="text-red-500 animate-pulse" />
+          <FaThermometerHalf  className="text-red-500 animate-pulse text-7xl max-lg:text-4xl " />
         </div>
         <div className="absolute bottom-5 left-5">
-          <GiWashingMachine size={50} className="text-gray-500 animate-pulse" />
+          <GiWashingMachine className="text-gray-500 animate-pulse text-7xl max-lg:text-4xl" />
         </div>
         <div className="absolute bottom-5 right-5">
-          <MdTv size={50} className="text-purple-500 animate-pulse" />
+          <MdTv className="text-purple-500 animate-pulse text-7xl max-lg:text-4xl" />
         </div>
         <div className="absolute top-[40%] left-2">
-          <MdOutlineCoffeeMaker size={50} color="brown" className="animate-pulse" />
+          <MdOutlineCoffeeMaker  color="brown" className="animate-pulse text-7xl max-lg:text-4xl" />
         </div>
         <div className="absolute bottom-[40%] right-2">
-          <PiSecurityCameraFill size={50} className="text-green-500 animate-pulse" />
+          <PiSecurityCameraFill  className="text-green-500 animate-pulse text-7xl max-lg:text-4xl" />
         </div>
       </div>
     </section>
